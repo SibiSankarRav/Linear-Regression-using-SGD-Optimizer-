2854c9766eb70c14fee55c9246ffb51c06ab909e
SibiSankarRav <56363789+SibiSankarRav@users.noreply.github.com>
2019-10-15 20:47:25 +0200

    Add files via upload

---
+++ gradientDescent.m
@@ -0,0 +1,48 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+size(X)
+for iter = 1:num_iters
+    
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCost) and gradient here.
+    %
+    
+%    theta=[theta(1);theta(2)];
+%    h=X*theta;
+%    diff=(h-y);
+
+%    omega_1=(1/m)*sum((diff).*X(1));
+%    omega_2=(1/m)*sum((diff).*X(2));
+%    omega=[omega_1;omega_2];
+
+%    theta=theta-alpha*omega;
+
+    theta=[theta(1);theta(2)];
+    h=X*theta;
+    diff=(h-y);
+    %diff2=(((h.^2)-(y.^2)).*(X(2).^2));
+    omega=(1/m)*[sum((diff).*(X(1))); sum((diff).*X(2))];
+    theta=theta-alpha*omega;
+    
+
+
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCost(X, y, theta);
+    
+end
+disp(J_history)
+end
